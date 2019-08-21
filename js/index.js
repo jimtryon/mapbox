d9e8e649da604d1f5cd62d347562e6282d1b8925
Jim Tryon <tryonjf@comcast.net>
2019-08-21 09:18:36 -0700

    Add css to HTML file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamltLXQiLCJhIjoiY2p6bGFwdmJxMGN3NjNjbzNvMHAzeGhwcyJ9.EVV9I4Bj8bS-B0d2PnB5rA';
+var secretKey = config.KEY; 
+
+mapboxgl.accessToken = secretKey;
 var map = new mapboxgl.Map({
     container: 'map', // Container ID
     style: 'mapbox://styles/mapbox/streets-v10', // Map style to use
-    center: [47.608013, -90], // Starting position [lat, long]
+    center: [-74.5, 40], // Starting position [lat, long]
+    zoom: 9 // Starting zoom
 });
